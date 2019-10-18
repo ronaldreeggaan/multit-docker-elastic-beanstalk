515ca4ca2b69a9c78d9cae625ee31f5b827bc520
Ronald Reegan Mariya Innasi <ronald.reegan.mariya.innasi@sap.com>
2019-10-18 08:31:58 +0530

    initial push

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import OtherPage from './OtherPage';
+import Fib from './Fib'
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <h1>Fib Calculator V2</h1>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+
+    
+  );
+}
+
+export default App;
